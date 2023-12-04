448b30a4a5db35ed91e198d26a7cf924abee7795
mnicolas94 <miguelcok27@gmail.com>
2023-12-04 15:47:16 -0500

    feat: implement script to set fixed width

---
+++ Assets/Scripts/Utility/CameraFixedWidthSetter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using Utils.Attributes;
+
+namespace Utility
+{
+    public class CameraFixedWidthSetter : MonoBehaviour
+    {
+        [SerializeField] private float _targetWidth;
+        [SerializeField, AutoProperty(AutoPropertyMode.Scene)] private Camera _camera;
+
+        private void Awake()
+        {
+            SetHeight();
+        }
+
+        [ContextMenu(nameof(SetHeight))]
+        private void SetHeight()
+        {
+            var aspect = _camera.aspect;
+            var targetHeight = _targetWidth / aspect;
+            _camera.orthographicSize = targetHeight / 2;
+        }
+    }
+}
