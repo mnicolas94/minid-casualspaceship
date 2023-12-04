47c968afa31ab646f3bebcfbecde633f23e30af5
mnicolas94 <miguelcok27@gmail.com>
2023-12-04 15:32:53 -0500

    feat: implement movement and screen vertical limits teleport

---
+++ Assets/Scripts/Input/MoveInput.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Input
+{
+    public class MoveInput : MonoBehaviour
+    {
+        [SerializeField] private Movement _movement;
+        [SerializeField] private InputActionReference _inputActionReference;
+
+        private InputAction _inputAction;
+
+        private void Awake()
+        {
+            _inputAction = _inputActionReference.action;
+        }
+
+        private void OnEnable()
+        {
+            _inputAction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputAction.Disable();
+        }
+
+        private void Update()
+        {
+            var inputValue = _inputAction.ReadValue<float>();
+
+            var pressed = inputValue > 0.5f;
+
+            if (pressed)
+            {
+                _movement.MoveUp();
+            }
+            else
+            {
+                _movement.MoveDown();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] private float _maxSpeed;
+        [SerializeField] private float _lerpSpeed;
+        [SerializeField] private Camera _camera;
+
+        private float _targetSpeed;
+        private float _currentSpeed;
+
+        public void Move(float dir)
+        {
+            dir = Mathf.Clamp(dir, -1, 1);
+            _targetSpeed = dir * _maxSpeed;
+        }
+        
+        public void MoveUp()
+        {
+            Move(1);
+        }
+
+        public void MoveDown()
+        {
+            Move(-1);
+        }
+
+        private void FixedUpdate()
+        {
+            // accelerate
+            _currentSpeed = Mathf.Lerp(_currentSpeed, _targetSpeed, _lerpSpeed);
+
+            // calculate position
+            var tr = transform;
+            var position = tr.position;
+            position.y += _currentSpeed * Time.fixedDeltaTime;
+            
+            // check limits
+            var screenHeight = _camera.orthographicSize;
+            var cameraPosition = _camera.transform.position;
+            var topLimit = cameraPosition.y + screenHeight;
+            var botLimit = cameraPosition.y - screenHeight;
+            
+            if (position.y > topLimit)
+            {
+                var offset = position.y - topLimit;
+                position.y = botLimit + offset;
+            }
+            
+            if (position.y < botLimit)
+            {
+                var offset = position.y - botLimit;
+                position.y = topLimit + offset;
+            }
+            
+            // update position
+            tr.position = position;
+        }
+    }
+}
