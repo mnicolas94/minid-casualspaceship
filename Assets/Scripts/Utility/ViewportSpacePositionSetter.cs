75dd9752f784303154512cc36a2d7b09d53c9f86
mnicolas94 <miguelcok27@gmail.com>
2023-12-04 16:00:36 -0500

    feat: set spaceship position fixed in viewport space

---
+++ Assets/Scripts/Utility/ViewportSpacePositionSetter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utility
+{
+    public class ViewportSpacePositionSetter : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+        [SerializeField] private Transform _target;
+        [SerializeField] private Vector2 _position;
+        
+        private void Awake()
+        {
+            SetPosition();
+        }
+
+        [ContextMenu(nameof(SetPosition))]
+        private void SetPosition()
+        {
+            var worldPoint = _camera.ViewportToWorldPoint(_position);
+            worldPoint.z = _target.position.z;
+            _target.position = worldPoint;
+        }
+    }
+}
