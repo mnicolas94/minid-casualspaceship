7b094d7fa8213f576b299ad35bdfc971137d829d
mnicolas94 <miguelcok27@gmail.com>
2023-12-05 12:57:34 -0500

    feat: implement score

---
+++ Assets/Scripts/Score/ScoreUpdater.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace Score
+{
+    public class ScoreUpdater : MonoBehaviour
+    {
+        [SerializeField] private FloatReference _score;
+        [SerializeField] private FloatReference _baseSpeed;
+        [SerializeField] private FloatReference _speedToScoreMultiplier;
+
+        private bool _isRunning;
+
+        private void Start()
+        {
+            StartUpdating();
+        }
+
+        public void StartUpdating()
+        {
+            _isRunning = true;
+        }
+
+        public void StopUpdating()
+        {
+            _isRunning = false;
+        }
+        
+        private void Update()
+        {
+            if (_isRunning)
+            {
+                _score.Value += _baseSpeed * _speedToScoreMultiplier * Time.deltaTime;
+            }
+        }
+    }
+}
