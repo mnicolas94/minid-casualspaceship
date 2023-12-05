aa5c13ccd4eccf796ba00f26af49efdde1420815
mnicolas94 <miguelcok27@gmail.com>
2023-12-05 12:47:03 -0500

    feat: implement currency

---
+++ Assets/Scripts/Currency/CollectableCurrency.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.Events;
+using Utils.Extensions;
+
+namespace Currency
+{
+    public class CollectableCurrency : MonoBehaviour
+    {
+        [SerializeField] private IntReference _toCollect;
+        [SerializeField] private IntReference _currencyCollectedCount;
+        [SerializeField] private LayerMask _playerLayer;
+
+        [SerializeField] private UnityEvent _onCollected;
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (_playerLayer.IsLayerInMask(col.gameObject.layer))
+            {
+                _currencyCollectedCount.Value += _toCollect;
+                
+                _onCollected.Invoke();
+                
+                // disable
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
