7a4061594c5de11cac763d7a253606b93f1ddf46
mnicolas94 <miguelcok27@gmail.com>
2023-12-11 10:15:36 -0500

    chore: install solid alloy's packages and test generic scriptable objects

---
+++ Assets/Scripts/Spawning/TestGeneric.cs
@@ -0,0 +1,12 @@
+﻿using GenericUnityObjects;
+using UnityEngine;
+
+namespace Spawning
+{
+    // [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    [CreateGenericAssetMenu]
+    public class TestGeneric<T> : GenericScriptableObject
+    {
+        [SerializeField] private T _generic;
+    }
+}
