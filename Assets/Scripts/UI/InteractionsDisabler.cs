d594e373cf88f7c7043e238f246a38babca91b99
mnicolas94 <miguelcok27@gmail.com>
2023-12-11 11:49:45 -0500

    feat: create popups for stores and options menu

---
+++ Assets/Scripts/UI/InteractionsDisabler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using Utils.Attributes;
+
+namespace UI
+{
+    public class InteractionsDisabler : MonoBehaviour
+    {
+        [SerializeField, AutoProperty(AutoPropertyMode.Parent)] private CanvasGroup _canvasGroup;
+
+        private void OnEnable()
+        {
+            _canvasGroup.interactable = true;
+            _canvasGroup.blocksRaycasts = true;
+        }
+
+        public void DisableInteractions()
+        {
+            _canvasGroup.interactable = false;
+            _canvasGroup.blocksRaycasts = false;
+        }
+    }
+}
