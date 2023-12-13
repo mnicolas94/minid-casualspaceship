cd9bffa330e1d709676e9480ddf44ca147cfb0d9
mnicolas94 <miguelcok27@gmail.com>
2023-12-13 08:59:02 -0500

    feat: update score record and currencies on game over screen

---
+++ Assets/Scripts/Currency/GameOverCurrenciesUpdater.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using SaveSystem;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace Currency
+{
+    public class GameOverCurrenciesUpdater : MonoBehaviour
+    {
+        [SerializeField] private IntReference _currentSoftCurrency;
+        [SerializeField] private IntReference _currentHardCurrency;
+        [SerializeField] private IntVariable _totalSoftCurrency;
+        [SerializeField] private IntVariable _totalHardCurrency;
+
+        private void OnEnable()
+        {
+            CollectCurrencies();
+        }
+
+        private async void CollectCurrencies()
+        {
+            _totalSoftCurrency.Value += _currentSoftCurrency;
+            _totalHardCurrency.Value += _currentHardCurrency;
+            
+            _currentSoftCurrency.Value = 0;
+            _currentHardCurrency.Value = 0;
+            
+            await _totalSoftCurrency.Save();
+            await _totalHardCurrency.Save();
+        }
+    }
+}
---
+++ Assets/Scripts/Score/RecordUpdater.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SaveSystem;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace Score
+{
+    public class RecordUpdater : MonoBehaviour
+    {
+        [SerializeField] private FloatReference _score;
+        [SerializeField] private FloatVariable _record;
+
+        private void OnEnable()
+        {
+            UpdateRecord();
+        }
+
+        private async void UpdateRecord()
+        {
+            if (_score.Value > _record.Value)
+            {
+                _record.Value = _score;
+                await _record.Save();
+            }
+        }
+    }
+}
