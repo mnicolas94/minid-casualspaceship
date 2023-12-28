9d53e872ee007303c9d38dd9f736c3337bbab524
mnicolas94 <miguelcok27@gmail.com>
2023-12-28 16:29:16 -0500

    feat: setup missions manager and implement a simple mission

--- Assets/Scripts/Currency/Currency.cs
+++ Assets/Scripts/Currency/Currency.cs
@@ -7,9 +7,16 @@ namespace Currency
     public class Currency : ScriptableObject
     {
         [SerializeField] private Sprite _sprite;
-        [SerializeField] private IntVariable _variable;
-
         public Sprite Sprite => _sprite;
+        
+        [SerializeField] private IntVariable _variable;
         public IntVariable Variable => _variable;
+        
+        /// <summary>
+        /// currency count collected during a game. On game over, this amount is added to the actual/total
+        /// currency variable.
+        /// </summary>
+        [SerializeField] private IntVariable _currentInGame;
+        public IntVariable CurrentInGame => _currentInGame;
     }
 }
---
+++ Assets/Scripts/Missions/MissionCollectXCoins.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using SaveSystem.Attributes;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace Missions
+{
+    [Serializable]
+    public class MissionCollectXCoins : BaseMission
+    {
+        [SerializeField, DoNotPersist] private Currency.Currency _currency;
+        [SerializeField, DoNotPersist] private float _goal;
+        
+        [SerializeField] private int _currentCollected;
+        
+        public override void Initialize()
+        {
+            _currentCollected = 0;
+        }
+
+        public override void StartMission()
+        {
+            _currency.CurrentInGame.ChangedWithHistory.Register(OnCurrencyCollected);
+        }
+
+        public override void EndMission()
+        {
+            _currency.CurrentInGame.ChangedWithHistory.Unregister(OnCurrencyCollected);
+        }
+
+        private void OnCurrencyCollected(IntPair tuple)
+        {
+            var (oldValue, newValue) = tuple;
+            var difference = newValue - oldValue;
+            var didIncrease = difference > 0;
+            if (didIncrease)
+            {
+                _currentCollected += difference;
+                if (_currentCollected >= _goal)
+                {
+                    OnCompleted?.Invoke();
+                }
+            }
+        }
+    }
+}
