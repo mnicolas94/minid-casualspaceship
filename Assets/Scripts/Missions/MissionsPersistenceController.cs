2165fb7846d80316b6655a61c67330e20990b7b6
mnicolas94 <miguelcok27@gmail.com>
2023-12-28 16:35:04 -0500

    feat: save missions list on changed

---
+++ Assets/Scripts/Missions/MissionsPersistenceController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SaveSystem;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Missions
+{
+    public class MissionsPersistenceController : MonoBehaviour
+    {
+        [SerializeField] private MissionsSerializableList _currentMissions;
+
+        [SerializeField] private UnityEvent _onLoaded;
+
+        private async void Start()
+        {
+            await _currentMissions.LoadOrCreate();
+            _onLoaded.Invoke();
+
+            _currentMissions.Added += OnChanged;
+            _currentMissions.Removed += OnChanged;
+        }
+
+        private void OnDestroy()
+        {
+            _currentMissions.Added -= OnChanged;
+            _currentMissions.Removed -= OnChanged;
+        }
+
+        private async void OnChanged(IMission mission)
+        {
+            await _currentMissions.Save();
+        }
+    }
+}
