cc8d8ee96910b9403743c4e59a5cefec23d9843e
mnicolas94 <miguelcok27@gmail.com>
2024-01-04 11:07:35 -0500

    feat: display ads with crazy games sdk on webgl builds

---
+++ Assets/Scripts/Services/CrazyGamesIntegration/CrazyGamesAdBreak.cs
@@ -0,0 +1,31 @@
+﻿using CrazyGames;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Services.CrazyGamesIntegration
+{
+    public class CrazyGamesAdBreak : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _onAdBreakCompleted;
+        [SerializeField] private UnityEvent _onAdBreakError;
+        
+        [SerializeField] private UnityEvent _onAdBreakRewardedCompleted;
+        [SerializeField] private UnityEvent _onAdBreakRewardedError;
+        
+        public void BeginAdBreak()
+        {
+            CrazyAds.Instance.beginAdBreak(
+                () => _onAdBreakCompleted.Invoke(),
+                () => _onAdBreakError.Invoke()
+                );
+        }
+
+        public void BeginAdBreakRewarded()
+        {
+            CrazyAds.Instance.beginAdBreakRewarded(
+                () => _onAdBreakRewardedCompleted.Invoke(),
+                () => _onAdBreakRewardedError.Invoke()
+            );
+        }
+    }
+}
