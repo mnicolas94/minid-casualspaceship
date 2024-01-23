d0b55ec2b27e9ddc2327966c0fdc7e3ed682202b
mnicolas94 <miguelcok27@gmail.com>
2024-01-23 15:45:51 -0500

    feat(ui): display currency icon in missing places

---
+++ Assets/Scripts/Currency/UI/CurrencyUiText.cs
@@ -0,0 +1,46 @@
+﻿using TMPro;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Currency.UI
+{
+    public enum CurrencyContext
+    {
+        Total,
+        InGame,
+    }
+    
+    public class CurrencyUiText : MonoBehaviour
+    {
+        [SerializeField] private Currency _currency;
+        [SerializeField] private CurrencyContext _currencyContext;
+        [SerializeField] private Image _currencyIcon;
+        [SerializeField] private TextMeshProUGUI _text;
+
+        private IntVariable Variable =>
+            _currencyContext == CurrencyContext.Total ? _currency.Variable : _currency.CurrentInGame;
+
+        private void OnEnable()
+        {
+            UpdateUi();
+            Variable.Changed.Register(UpdateText);
+        }
+
+        private void OnDisable()
+        {
+            Variable.Changed.Unregister(UpdateText);
+        }
+
+        private void UpdateUi()
+        {
+            _currencyIcon.sprite = _currency.Sprite;
+            UpdateText();
+        }
+
+        private void UpdateText()
+        {
+            _text.text = Variable.Value.ToString();
+        }
+    }
+}
