c59e09d4fdf957b2e37d93b16ca0248e356bbd91
mnicolas94 <miguelcok27@gmail.com>
2024-01-25 15:54:42 -0500

    feat(cicd): implement builds preprocessor to set the webgl template with Deploy's override variables feature

---
+++ Assets/Scripts/Editor/WebGLTemplateBuildsSelection/SerializableWebGLTemplate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Utils.Attributes;
+
+namespace Editor.WebGLTemplateBuildsSelection
+{
+    [CreateAssetMenu(fileName = "SerializableWebGLTemplate", menuName = "SerializableWebGLTemplate", order = 0)]
+    public class SerializableWebGLTemplate : ScriptableObject
+    {
+        [SerializeField, Dropdown(nameof(GetTemplates))] private string _template;
+
+        public string Template => _template;
+
+        private DropdownList<string> GetTemplates()
+        {
+            var templates = new DropdownList<string>();
+            
+            templates.Add("Crazy2020", "PROJECT:Crazy_2020");
+            templates.Add("Crazy5.6", "PROJECT:Crazy_5_6+");
+            templates.Add("Default", "APPLICATION:Default");
+            templates.Add("Minimal", "APPLICATION:Minimal");
+            templates.Add("PWA", "APPLICATION:PWA");
+
+            return templates;
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/WebGLTemplateBuildsSelection/WebglTemplatePreprocessor.cs
@@ -0,0 +1,20 @@
+﻿using Deploy.Runtime;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Editor.WebGLTemplateBuildsSelection
+{
+    public class WebglTemplatePreprocessor : IPreprocessBuildWithReport
+    {
+        public int callbackOrder { get; }
+        
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            var templateVariable = Resources.Load<SerializableWebGLTemplate>("WebGLTemplateReference");
+            Debug.Log("templateVariable.Value = " + templateVariable.Template);
+            PlayerSettings.WebGL.template = templateVariable.Template;
+        }
+    }
+}
